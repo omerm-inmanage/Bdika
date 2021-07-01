2dbcdc026c7b6b5bf18a44e59797ec22e1b3db67
Omer Cohen <>
2021-07-01 09:34:39 +0300

    no message

---
+++ Bdika/Bdika/Bdika.h
@@ -0,0 +1,18 @@
+//
+//  Bdika.h
+//  Bdika
+//
+//  Created by Omer Cohen on 01/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bdika.
+FOUNDATION_EXPORT double BdikaVersionNumber;
+
+//! Project version string for Bdika.
+FOUNDATION_EXPORT const unsigned char BdikaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bdika/PublicHeader.h>
+
+
